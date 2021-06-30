5c2051e69118a14ae9966145148c8f70f52510cc
Carlos-Reinheimer <carlosgreinheimer@gmail.com>
2021-06-30 00:00:51 -0300

    onDrawGizmos, Manager and Editor preprocessors

---
+++ Tool Dev Study/Assets/Scripts/ExplosiveBarrel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// ExecuteAlways - all the functions are also gonna be called in the editor (like onEnable())
+[ExecuteAlways]
+public class ExplosiveBarrel : MonoBehaviour
+{
+    // define variables that are gonna be used 
+
+    // TIP: use a slider to make sure the person who is gonna use it (like a level designer) doesn't make mistakes
+    [Range(1f, 8f)]
+    public float radius = 4f;
+    
+    public float damage = 10f;
+    public Color color = Color.red;
+
+    // adding it self to the list of allBarrels in the manager
+    void OnEnable() => ExplosiveBarrelsManager.allBarrels.Add(this);
+    // removing it self of the list of allBarrels in the manager
+    void OnDisable() => ExplosiveBarrelsManager.allBarrels.Remove(this);
+    
+       
+
+    
+
+
+    //onDrawGizmos - helps who is using the gameObject to visualize what's is hapenning
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
---
+++ Tool Dev Study/Assets/Scripts/ExplosiveBarrelsManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// only using UnityEditor in some sort of dev enviroment or something
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class ExplosiveBarrelsManager : MonoBehaviour
+{
+    // having a script who knows all the other barrels that exists
+    public static List<ExplosiveBarrel> allBarrels = new List<ExplosiveBarrel>();
+
+    // sabe if in the funciton
+    private void OnDrawGizmos()
+    {
+    foreach (ExplosiveBarrel barrel in allBarrels)
+	{
+            // using Handles is better beacause
+            // used when dragging things around
+            // Handles is a editor only class 
+            #if UNITY_EDITOR
+            Handles.DrawAAPolyLine(transform.position, barrel.transform.position);
+            #endif
+
+            //Gizmos.DrawLine(transform.position, barrel.transform.position);
+        }
+
+    }
+
+}
