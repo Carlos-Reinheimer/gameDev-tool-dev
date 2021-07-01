b9852e2869cf29204fb8380504999a79b1170dfd
Carlos-Reinheimer <carlosgreinheimer@gmail.com>
2021-06-30 23:51:27 -0300

    MatPropBlock, handles, etc

--- Tool Dev Study/Assets/Scripts/ExplosiveBarrel.cs
+++ Tool Dev Study/Assets/Scripts/ExplosiveBarrel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 
 // ExecuteAlways - all the functions are also gonna be called in the editor (like onEnable())
@@ -16,6 +17,53 @@ public class ExplosiveBarrel : MonoBehaviour
     public float damage = 10f;
     public Color color = Color.red;
 
+    MaterialPropertyBlock mpb;
+
+    // Optimize with PropertyToId witch converts a string to a integer (?)
+    static readonly int shPropColor = Shader.PropertyToID("_Color");
+    public MaterialPropertyBlock Mpb
+    {
+        get
+        {
+            if (mpb == null) mpb = new MaterialPropertyBlock();
+            return mpb;
+        }
+    }
+
+    void ApplyColor()
+    {
+        MeshRenderer rnd = GetComponent<MeshRenderer>();
+        Mpb.SetColor(shPropColor, color);
+        rnd.SetPropertyBlock(Mpb); // setting the Propertie Block to the mesh
+
+        //rnd.material.SetColor("_Color", color); ==== rnd.material.color(color);
+    }
+    private void OnValidate()
+    {
+        // This function is called every time some value has changed in the inspector
+        ApplyColor();
+    }
+
+    //private void Awake()
+    //{
+    //    // ------------------------------------------------------------------------------------------
+    //    Shader shader = Shader.Find("Default/Diffuse");
+    //    // This is an asset, witch will exist until you say to destroy it. It can cause memory leaks 
+    //    Material material = new Material(shader){ hideFlags = HideFlags.HideAndDontSave };
+    //    // ------------------------------------------------------------------------------------------
+
+
+    //    // ---------- DON'T DO ----------
+
+    //    // This is will duplicate the material/mesh witch is bad
+    //    //GetComponent<MeshRenderer>().material.color = Color.red;
+    //    //GetComponent<MeshFilter>().mesh.vertices = ...
+
+    //    // This is will modify the proper material/mesh, witch is also bad
+    //    //GetComponent<MeshRenderer>().sharedMaterial.color = Color.red;
+    //    //GetComponent<MeshFilter>().sharedMesh.vertices = ...
+    //}
+
     // adding it self to the list of allBarrels in the manager
     void OnEnable() => ExplosiveBarrelsManager.allBarrels.Add(this);
     // removing it self of the list of allBarrels in the manager
@@ -29,6 +77,8 @@ public class ExplosiveBarrel : MonoBehaviour
     //onDrawGizmos - helps who is using the gameObject to visualize what's is hapenning
     private void OnDrawGizmosSelected()
     {
-        Gizmos.DrawWireSphere(transform.position, radius);
+        Handles.DrawWireDisc(transform.position, transform.up, radius);
+
+        //Gizmos.DrawWireSphere(transform.position, radius);
     }
 }
--- Tool Dev Study/Assets/Scripts/ExplosiveBarrelsManager.cs
+++ Tool Dev Study/Assets/Scripts/ExplosiveBarrelsManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 // only using UnityEditor in some sort of dev enviroment or something
 #if UNITY_EDITOR
@@ -13,20 +14,33 @@ public class ExplosiveBarrelsManager : MonoBehaviour
     public static List<ExplosiveBarrel> allBarrels = new List<ExplosiveBarrel>();
 
     // sabe if in the funciton
+    #if UNITY_EDITOR
     private void OnDrawGizmos()
     {
+
+        // Dealing with how the lines works
+        Handles.zTest = CompareFunction.LessEqual;
+
     foreach (ExplosiveBarrel barrel in allBarrels)
 	{
+
+            Vector3 managerPos = transform.position;
+            Vector3 barrelPos = barrel.transform.position;
+            float halfHeight = (managerPos.y - barrelPos.y) * 0.5f;
+            Vector3 tangentOffset = Vector3.up * halfHeight;
+
+            Handles.DrawBezier(managerPos, barrelPos, managerPos - tangentOffset, barrelPos + tangentOffset, Color.white, EditorGUIUtility.whiteTexture, 1f) ;
+
             // using Handles is better beacause
             // used when dragging things around
             // Handles is a editor only class 
-            #if UNITY_EDITOR
-            Handles.DrawAAPolyLine(transform.position, barrel.transform.position);
-            #endif
+
+            //Handles.DrawAAPolyLine(transform.position, barrel.transform.position);
 
             //Gizmos.DrawLine(transform.position, barrel.transform.position);
         }
 
     }
+    #endif
 
 }
