a415b11ac982fc8281cb76e1690026c34005475c
Carlos-Reinheimer <carlosgreinheimer@gmail.com>
2021-07-05 23:06:12 -0300

    quest 2

--- Tool Dev Study/Assets/Editor/SnapperTool.cs
+++ Tool Dev Study/Assets/Editor/SnapperTool.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEditor;
 
 public class SnapperTool : EditorWindow {
@@ -8,13 +9,16 @@ public class SnapperTool : EditorWindow {
     [MenuItem("Tools/Snapper Tool")]
     public static void OpenTheThing() => GetWindow<SnapperTool>("Snapper"); // single window 
     public float gridSize = 1f;
+    public Vector3 point;
 
     SerializedObject so;
     SerializedProperty propGridSize;
+    SerializedProperty propPoint;
 
     private void OnEnable() {
         so = new SerializedObject(this);
         propGridSize = so.FindProperty("gridSize");
+        propPoint = so.FindProperty("point");
 
         Selection.selectionChanged += Repaint; // update the window when selection change
         SceneView.duringSceneGui += DuringSceneGUI;
@@ -24,7 +28,34 @@ public class SnapperTool : EditorWindow {
         SceneView.duringSceneGui -= DuringSceneGUI;
     }
     void DuringSceneGUI(SceneView scene) {
-        Handles.DrawLine(Vector3.zero, Vector3.up);
+
+        so.Update();
+        propPoint.vector3Value = Handles.PositionHandle(point, Quaternion.identity);
+        so.ApplyModifiedProperties();
+
+        //if (Event.current.type == EventType.Repaint) {
+        Handles.zTest = CompareFunction.LessEqual;
+
+        const float gridDrawExtent = 16;
+        int lineCount = Mathf.RoundToInt((gridDrawExtent * 2) / gridSize);
+        if (lineCount % 2 == 0) lineCount++; // make sure it's a odd number!
+        int halfLineCount = lineCount / 2;
+
+        for (int i = 0; i < lineCount; i++) {
+            int intOffset = i - halfLineCount;
+
+            float xCoord = intOffset * gridSize;
+            float zCoord0 = halfLineCount * gridSize;
+            float zCoord1 = -halfLineCount * gridSize;
+            Vector3 p0 = new Vector3(xCoord, 0f, zCoord0);
+            Vector3 p1 = new Vector3(xCoord, 0f, zCoord1);
+            Handles.DrawAAPolyLine(p0, p1);
+            p0 = new Vector3(zCoord0, 0f, xCoord);
+            p1 = new Vector3(zCoord1, 0f, xCoord);
+            Handles.DrawAAPolyLine(p0, p1);
+        }
+
+        //}
     }
 
     private void OnGUI() {
