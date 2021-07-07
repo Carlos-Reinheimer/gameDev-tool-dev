69bb32f1b0926f4ce55673b2df2874eca6ea731e
Carlos-Reinheimer <carlosgreinheimer@gmail.com>
2021-07-07 00:01:42 -0300

    draw and snap polar grid

--- Tool Dev Study/Assets/Editor/SnapperTool.cs
+++ Tool Dev Study/Assets/Editor/SnapperTool.cs
@@ -6,19 +6,32 @@ using UnityEditor;
 
 public class SnapperTool : EditorWindow {
 
+    public enum GridType {
+        Cartesian,
+        Polar
+    }
+
     [MenuItem("Tools/Snapper Tool")]
     public static void OpenTheThing() => GetWindow<SnapperTool>("Snapper"); // single window 
+    const float TAU = 6.28318530718f;
     public float gridSize = 1f;
+    public GridType gridType = GridType.Cartesian;
+    public int angularDivision = 24;
+
     public Vector3 point;
 
     SerializedObject so;
     SerializedProperty propGridSize;
     SerializedProperty propPoint;
+    SerializedProperty propGridType;
+    SerializedProperty propAngularDivision;
 
     private void OnEnable() {
         so = new SerializedObject(this);
         propGridSize = so.FindProperty("gridSize");
         propPoint = so.FindProperty("point");
+        propGridType = so.FindProperty("gridType");
+        propAngularDivision = so.FindProperty("angularDivision");
 
         Selection.selectionChanged += Repaint; // update the window when selection change
         SceneView.duringSceneGui += DuringSceneGUI;
@@ -35,13 +48,41 @@ public class SnapperTool : EditorWindow {
 
         //if (Event.current.type == EventType.Repaint) {
         Handles.zTest = CompareFunction.LessEqual;
-
         const float gridDrawExtent = 16;
+
+        if (gridType == GridType.Cartesian) DrawGridCartesian(gridDrawExtent);
+        else DrawGridPolar(gridDrawExtent);
+
+
+        //}
+    }
+    
+    void DrawGridPolar(float gridDrawExtent) {
+        int ringCount = Mathf.RoundToInt(gridDrawExtent / gridSize);
+        float radiusOuter = (ringCount -1) * gridSize;
+
+        // radial grid (rings)
+        for (int i = 1; i < ringCount; i++) Handles.DrawWireDisc(Vector3.zero, Vector3.up, i * gridSize);
+
+        // angular grid (lines)
+        for (int i = 0; i < angularDivision; i++) {
+            float t = i / (float)angularDivision;
+            float angRad = t * TAU; // turns to radians
+            float x = Mathf.Cos(angRad);
+            float y = Mathf.Sin(angRad);
+            Vector3 dir = new Vector3(x, 0f, y);
+
+            Handles.DrawAAPolyLine(Vector3.zero, dir * radiusOuter);
+        }
+    }
+
+    void DrawGridCartesian(float gridDrawExtent) {
         int lineCount = Mathf.RoundToInt((gridDrawExtent * 2) / gridSize);
         if (lineCount % 2 == 0) lineCount++; // make sure it's a odd number!
         int halfLineCount = lineCount / 2;
 
-        for (int i = 0; i < lineCount; i++) {
+        for (int i = 0; i < lineCount; i++)
+        {
             int intOffset = i - halfLineCount;
 
             float xCoord = intOffset * gridSize;
@@ -54,14 +95,17 @@ public class SnapperTool : EditorWindow {
             p1 = new Vector3(zCoord1, 0f, xCoord);
             Handles.DrawAAPolyLine(p0, p1);
         }
-
-        //}
     }
 
     private void OnGUI() {
 
         so.Update();
+        EditorGUILayout.PropertyField(propGridType);
         EditorGUILayout.PropertyField(propGridSize);
+        if (gridType == GridType.Polar) {
+            EditorGUILayout.PropertyField(propAngularDivision);
+            propAngularDivision.intValue = Mathf.Max(4, propAngularDivision.intValue);
+        }
         so.ApplyModifiedProperties();
 
         //using (new GUILayout.HorizontalScope()) {
@@ -80,7 +124,28 @@ public class SnapperTool : EditorWindow {
     void SnapSelection() {
         foreach (GameObject go in Selection.gameObjects) {
             Undo.RecordObject(go.transform, "snap objects");
-            go.transform.position = go.transform.position.Round(gridSize);
+            go.transform.position = GetSnappedPosition(go.transform.position);
         }
+    }
+
+    Vector3 GetSnappedPosition(Vector3 posOriginal){
+        if (gridType == GridType.Cartesian) return posOriginal.Round(gridSize);
+        
+        if (gridType == GridType.Polar) {
+            Vector2 vec = new Vector2(posOriginal.x, posOriginal.z);
+            float dist = vec.magnitude;
+            float distSnapped = dist.Round(gridSize);
+
+            float angRad = Mathf.Atan2(vec.y, vec.x); // 0 to TAU
+            float angTurns = angRad / TAU; // 0 to 1
+            float angSnapped = angTurns.Round(1f / angularDivision) * TAU;
+
+            Vector2 dirSnapped = new Vector2(Mathf.Cos(angSnapped), Mathf.Sin(angSnapped));
+            Vector2 snappedVector = distSnapped * dirSnapped;
+
+            return new Vector3(snappedVector.x, posOriginal.y, snappedVector.y);
+        }
+
+        return default;
     }
 }
--- Tool Dev Study/Assets/Scripts/ExtensionMethods.cs
+++ Tool Dev Study/Assets/Scripts/ExtensionMethods.cs
@@ -14,4 +14,8 @@ public static class ExtensionMethods {
     public static Vector3 Round(this Vector3 v, float gridSize) {
         return (v / gridSize).Round() * gridSize;
     }
+
+    public static float Round(this float v, float gridSize) {
+        return Mathf.Round(v / gridSize) * gridSize;
+    }
 }
